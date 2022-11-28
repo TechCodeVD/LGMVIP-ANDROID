ccde9523679c2adff2fc95f8134b8cf4f586adde
TechCodeVD <112330535+TechCodeVD@users.noreply.github.com>
2022-11-28 23:55:49 +0530

    Add files via upload

---
+++ app/src/main/java/com/login/covid_19/MainActivity.java
@@ -0,0 +1,218 @@
+import android.database.DataSetObserver;
+import android.graphics.ColorSpace;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.login.covid_19.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    public static List<ColorSpace.Model> modelList = new ArrayList<>();
+    ColorSpace.Model model;
+    Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        fetchData();
+
+    }
+
+    private void fetchData() {
+        // The Link Through Which We Can Fetch Data
+        String url = "https://api.covid19india.org/state_district_wise.json";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    // Creating JSON Object
+                    JSONObject object = new JSONObject(response);
+
+                    // From that object we are fetching data
+                    JSONObject object1 = object.getJSONObject("Uttar Pradesh");
+                    JSONObject object2 = object1.getJSONObject("districtData");
+                    JSONObject object3 = object2.getJSONObject("Prayagraj");
+                    JSONObject object4 = object3.getJSONObject("delta");
+
+
+                    String active = object3.getString("active");
+                    String confirmed = object3.getString("confirmed");
+                    String deceased = object3.getString("deceased");
+                    String recovered = object3.getString("recovered");
+
+                    String confInc = object4.getString("confirmed");
+                    String confDec = object4.getString("deceased");
+                    String confRec = object4.getString("recovered");
+
+                    model = new ColorSpace.Model("Prayagraj", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Ballia");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new ColorSpace.Model("Ballia", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Lucknow");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new ColorSpace.Model("Lucknow", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Varanasi");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new ColorSpace.Model("Varanasi", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Agra");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new ColorSpace.Model("Agra", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    adapter = new Adapter(MainActivity.this, modelList) {
+                        @Override
+                        public void registerDataSetObserver(DataSetObserver observer) {
+
+                        }
+
+                        @Override
+                        public void unregisterDataSetObserver(DataSetObserver observer) {
+
+                        }
+
+                        @Override
+                        public int getCount() {
+                            return 0;
+                        }
+
+                        @Override
+                        public Object getItem(int position) {
+                            return null;
+                        }
+
+                        @Override
+                        public long getItemId(int position) {
+                            return 0;
+                        }
+
+                        @Override
+                        public boolean hasStableIds() {
+                            return false;
+                        }
+
+                        @Override
+                        public View getView(int position, View convertView, ViewGroup parent) {
+                            return null;
+                        }
+
+                        @Override
+                        public int getItemViewType(int position) {
+                            return 0;
+                        }
+
+                        @Override
+                        public int getViewTypeCount() {
+                            return 0;
+                        }
+
+                        @Override
+                        public boolean isEmpty() {
+                            return false;
+                        }
+                    };
+                    listView.setAdapter(adapter);
+
+                    // In case of error it will run
+                }   catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // In case of error it will run
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+}
